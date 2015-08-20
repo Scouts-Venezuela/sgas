8046b4a3fe8d8a89b9b5e6550f55e5fb4e0f49b2
Enner Pérez <ennerperez@gmail.com>
2015-08-20 00:15:00 -0430

    GeoNames Controller

--- default/app/controllers/geonames_controller.php
+++ default/app/controllers/geonames_controller.php
@@ -1,8 +1,14 @@
 <?php
-class GeonamesController extends RestController {
+
+/**
+ * Controller para el manejo de GeoNames
+ */
+class GeonamesController extends RestController 
+{
+	function __construct() {
+		$this->publicView = true;
+	}
 	public function getAll() {
 		$this->data = GeoNames::getGeoNames ();
 	}
 }
-
-?>
